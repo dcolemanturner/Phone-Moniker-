886e6942d88b556d5be2f63b765e8414fe71b965
Clark Nelson <nelsonc1991@siu.edu>
2014-01-09 23:34:39 -0600

    Filters out words from the dictionary that dont match the first button

--- meetup.rb
+++ meetup.rb
@@ -1,13 +1,7 @@
-# Handy array methods
-# -------------------
-# delete_if
-# collect
-#
-# input
-# 843-6398
-
 class PhoneNumber
-	@numbers = {
+
+	#class variables are available in the class only
+	@@numbers = {
 		'2' => ['a','b','c'], 
 		'3' => ['d','e','f'], 
 		'4' => ['g','h','i'], 
@@ -17,8 +11,8 @@ class PhoneNumber
 		'8' => ['t','u','v'], 
 		'9' => ['w','x','y','z']
 	}
-	four_word = ['next', 'sock']
-	three_word = ['the', 'new', 'for']
+	@@four_word = ['next', 'sock']
+	@@three_word = ['the', 'new', 'for']
 
 	def translatePhoneNum (phone_raw)
 		firstThree = []
@@ -26,9 +20,9 @@ class PhoneNumber
 
 		phone_raw.split("").each_with_index { |i, index|
 			if index < 3
-				firstThree[index] = numbers.fetch(i)
+				firstThree[index] = @@numbers.fetch(i)
 			else
-				lastFour[index-3] = numbers.fetch(i)
+				lastFour[index-3] = @@numbers.fetch(i)
 			end
 		}
 
@@ -37,49 +31,51 @@ class PhoneNumber
 	end
 
 	def find3Word input
-
 		a = input[0]
 		b = input[1]
 		c = input[2]
-		three_word_result = []
+		posible = []
 
-		a.each { |first| 
-
-			three_word.each { |key|
-				if key[0] == first[0]
-					continue
-				else
-					stop
+		puts "variable a: " + a.to_s
+		#start by looping through the whole dictionary
+		@@three_word.each { |dict|
+			#test the first letter
+			a.each { |first|
+				if first == dict[0]
+					posible.push(dict)
 				end
 			}
-
-			b.each { |second| 
-				c.each { |third| 
-					three_word_result += three_word.select { |num| 
-						num == first + second + third 
-					}
-				}
-			}
 		}
-		return three_word_result
+
+		# posible now contains all the words
+		# that start with the first number
+		posible.each { |dict|
+			b.each { |second|
+				#start removing instead of adding
+				if second != dict[1]
+					posible
+
+			}
+
+		}
+		if posible.length == 0
+				puts "Sorry no answers match!"
+		end
+		puts "the answers are: " + posible.to_s
+		return posible
 	end
-	# one letter words
-	# check first letter vs valid responces
 
-	word_bank = ['a','an']
 	def find4Word input
-		a=input[0]
-		b=input[1]
-		c=input[2]
-		d=input[3]
+		d=input[0]
+		e=input[1]
+		f=input[2]
+		g=input[3]
 		four_word_result = []
-		
-
-		a.each { |first| 
-			b.each { |second| 
-				c.each { |third| 
-					d.each { |fourth|
-						four_word_result += four_word.select { |num| 
+		d.each { |first| 
+			e.each { |second| 
+				f.each { |third| 
+					g.each { |fourth|
+						four_word_result += @@four_word.select { |num| 
 							num == first + second + third + fourth
 						}
 					}
@@ -88,11 +84,11 @@ class PhoneNumber
 		}
 		return four_word_result
 	end
-	#translatePhoneNum "8436398"
 	
 end
 
 example = PhoneNumber.new
+example.translatePhoneNum "8436398"
 # Examples:
 # ---------
 # the-next == 843-6398
