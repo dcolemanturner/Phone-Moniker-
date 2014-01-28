0feaaaf6970f7e2cdc329b47432ffad5f7f4a3a0
Clark Nelson <nelsonc1991@siu.edu>
2014-01-28 16:32:16 -0600

    Changes for fun
    added “neat” for bourbon to start working with grid
    Added a background image

--- one8hundred/assets/js/app.js
+++ one8hundred/assets/js/app.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	$(".flip-num").find("button.up").on("click", function(event){
 		var thisInput = $(this).parent().find("input");
 		thisInput[0].stepUp(1);
+		alert(thisInput);
 	});
 	$(".flip-num").find("button.down").on("click", function(event){
 		var thisInput = $(this).parent().find("input");
 		thisInput[0].stepDown(1);
 	});
+
 });
