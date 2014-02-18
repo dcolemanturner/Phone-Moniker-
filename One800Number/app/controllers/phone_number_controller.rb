2456a0da254a996293bd70b8e90bf827b045bd16
Derrick C.Turner <dcolemanturner@gmail.com>
2014-02-18 18:47:17 -0500

    Basic Rails / AJAX communication piped in

--- One800Number/app/controllers/phone_number_controller.rb
+++ One800Number/app/controllers/phone_number_controller.rb
@@ -5,18 +5,28 @@ class PhoneNumberController < ApplicationController
 
   def show
 		#class variables are available in the class only
-		phone_num = []
-		phone_num.push(params[:a])
-		phone_num.push(params[:b])
-		phone_num.push(params[:c])
-		phone_num.push(params[:d])
-		phone_num.push(params[:e])
-		phone_num.push(params[:f])
-		phone_num.push(params[:g])
-		phone_num.join
+		@phone_num = []
+		@phone_num.push(params[:a])
+		@phone_num.push(params[:b])
+		@phone_num.push(params[:c])
+		@phone_num.push(params[:d])
+		@phone_num.push(params[:e])
+		@phone_num.push(params[:f])
+		@phone_num.push(params[:g])
+		@phone_num.join
+
 		@phone_number = PhoneNumber.new
-		@answers = @phone_number.search(3, phone_num)
+		@answers = @phone_number.search(3, @phone_num)
 		@first_answers = @answers.fetch(0);
 		@second_answers = @answers.fetch(1);
+
+		puts "first answer Object: " + @first_answers.inspect
+		puts "Second answer Object: " + @second_answers.inspect
+		@phone_num = @phone_num.join
+
+		respond_to do |format|
+  			format.js   {}
+  		end
+
 	end
 end
